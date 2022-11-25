ecfe4f78a1350ce559532da514067b9f49804249
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2022-11-25 10:24:22 +0100

    add first JS

---
+++ tragaperras.js
@@ -0,0 +1,23 @@
+
+
+function insertarNombre(event){
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre").value;
+
+    if(nombre == "")
+        alert("Debes añadir tu nombre")
+
+    let opcion = "<li> " + nombre + "</li>";
+
+    let lista = document.getElementById("lista-nombres");
+    
+    lista.innerHTML += opcion;
+
+}
+
+function resetearLista(event){
+    HTMLFormElement.reset()
+
+    let resetear = document.getElementById(nombre).reset();
+}
